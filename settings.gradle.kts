92e6cb436ce8b2c0f1a2ebcd7ae98020cd2d321b
Lujia Sun <134198151+mickee0527@users.noreply.github.com>
2024-06-27 14:51:45 -0400

    1.0

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LujiaSum_COMP304_001_Hands-On_Test1_S24"
+include(":app")
+ 
