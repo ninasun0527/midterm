027a2d3c9379f9817bbb0f88e134aaae8890b541
Lujia Sun <134198151+mickee0527@users.noreply.github.com>
2024-06-27 14:58:43 -0400

    1.2 second activity add

---
+++ app/src/main/java/com/lujia/sun/SunActivity.kt
@@ -0,0 +1,22 @@
+// student number : 300726390
+
+package com.lujia.sun
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sun)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
