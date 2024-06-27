ab619e3520911e41f48a5b9f394d546dfe07fdbc
Lujia Sun <134198151+mickee0527@users.noreply.github.com>
2024-06-27 15:34:15 -0400

    1.4 activity main

--- app/src/main/java/com/lujia/sun/LujiaActivity.kt
+++ app/src/main/java/com/lujia/sun/LujiaActivity.kt
@@ -3,21 +3,20 @@
 package com.lujia.sun
 
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class LujiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSelect = findViewById<Button>(R.id.buttonSelectExercises)
+        buttonSelect.setOnClickListener {
+            val intent = Intent(this, SunActivity::class.java)
+            startActivity(intent)
         }
     }
 }
