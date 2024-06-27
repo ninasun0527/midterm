de5f580cd1e84e84f207ca5ae3d5ee6e678cf49f
Lujia Sun <134198151+mickee0527@users.noreply.github.com>
2024-06-27 16:41:20 -0400

    1.4 2nd activity

--- app/src/main/java/com/lujia/sun/SunActivity.kt
+++ app/src/main/java/com/lujia/sun/SunActivity.kt
@@ -3,20 +3,35 @@
 package com.lujia.sun
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sun)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonNext = findViewById<Button>(R.id.buttonNext)
+        val checkBoxLoseThings = findViewById<CheckBox>(R.id.checkBoxLoseThings)
+        val checkBoxRememberNames = findViewById<CheckBox>(R.id.checkBoxRememberNames)
+        val checkBoxLearnQuickly = findViewById<CheckBox>(R.id.checkBoxLearnQuickly)
+        val checkBoxTrackMultiple = findViewById<CheckBox>(R.id.checkBoxTrackMultiple)
+
+        buttonNext.setOnClickListener {
+            val selectedExercises = StringBuilder("Selected exercises:")
+
+            if (checkBoxLoseThings.isChecked) selectedExercises.append("\n- Lose things ")
+            if (checkBoxRememberNames.isChecked) selectedExercises.append("\n- Remember names")
+            if (checkBoxLearnQuickly.isChecked) selectedExercises.append("\n- Learn things quickly")
+            if (checkBoxTrackMultiple.isChecked) selectedExercises.append("\n- Keep track of multiple things")
+
+            if (selectedExercises.length > 20) {
+                Toast.makeText(this, selectedExercises.toString(), Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No exercise selected", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
